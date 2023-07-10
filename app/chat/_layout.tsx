67a29479bb40295856fcda9685d3b581a2da7464
unknown <eske17.ea@gmail.com>
2023-07-10 15:51:35 +0300

    auth backend integration

--- app/chat/_layout.tsx
+++ app/chat/_layout.tsx
@@ -2,21 +2,24 @@ import { Stack } from "expo-router";
 import { useEffect } from "react";
 import { StreamChat } from "stream-chat";
 import { OverlayProvider, Chat } from "stream-chat-expo";
+import { useAuth } from "../../src/context/auth";
 
 const API_KEY = "t77txc29cy35";
 const client = StreamChat.getInstance("t77txc29cy35");
 
 export default function ChatLayout() {
+  const { user } = useAuth();
+
   useEffect(() => {
     // connect authenticated user
     const connectUser = async () => {
       await client.connectUser(
         {
-          id: "boo",
-          name: "John Doe",
+          id: user.id.toString(),
+          name: user.name,
           image: "https://getstream.io/random_svg/?name=John",
         },
-        client.devToken("boo")
+        user.streamToken
       );
 
       const channel = client.channel("livestream", "public", {
